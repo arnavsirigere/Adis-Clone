00fce4a2c0e40b81c832aed009f6444667ed18d6
Arnav Sirigere <arnavsirigere@gmail.com>
2020-10-18 18:09:12 +0530

    :sparkles: new prefix

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ A discord bot for Adi's Discord Hut - https://discord.gg/akEfvH9
 
 ## Commands
 
-`!ping` - Pong!
+`#ping` - Pong!
 
-`!invite` - To get a invite link to the server
+`#invite` - To get a invite link to the server
 
-`!help` - To view all the available commands and what they do
+`#help` - To view all the available commands and what they do
 
 ## Admin Only Commands
 
 [NOTE: These commands can be run by anyone with the ADMIN_ROLE]
 
-`!giveaway` - Randomly pick a member from the server for the giveaway prize
+`#giveaway` - Randomly pick a member from the server for the giveaway prize
--- src/commands/help.js
+++ src/commands/help.js
@@ -4,10 +4,10 @@ async function help(message) {
   const embed = new MessageEmbed()
     .setColor('#2DD881')
     .setTitle('Here are the commands you can use  - ')
-    .addField('`' + '!ping' + '`', 'Pong!')
-    .addField('`' + '!invite' + '`', 'To get a invite link to the server')
-    .addField('`' + '!help' + '`', 'Well, what you just used!')
-    .addField('`' + '!giveaway' + '`', 'To randomly pick a member from the server for the giveaway prize! [Admin only]');
+    .addField('`' + '#ping' + '`', 'Pong!')
+    .addField('`' + '#invite' + '`', 'To get a invite link to the server')
+    .addField('`' + '#help' + '`', 'Well, what you just used!')
+    .addField('`' + '#giveaway' + '`', 'To randomly pick a member from the server for the giveaway prize! [Admin only]');
 
   await message.channel.send(embed);
 }
